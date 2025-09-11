b7df07e3319c565d65f46447174fe3f4a2b6c249
atmukesh01 <iammukesh1075@gmail.com>
2025-09-11 19:12:21 +0530

    modified file About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -72,15 +72,15 @@ const About = () => {
         <LoadingName />
       ) : (
         <>
-          <nav className="navbar">
-            <div className="nav-right">
-              <a href="#about" className="nav-link">About</a>
-              <a href="#skills" className="nav-link">Skillset</a>
-              <a href="#projects" className="nav-link">Projects</a>
-              <a href="#contact" className="nav-link">Contact</a>
-            </div>
-            
-          </nav>
+          <div class="navbar">
+  <div class="logo">Logo</div>
+  <nav class="nav-right">
+    <a href="#" class="active">Home</a>
+    <a href="#">About</a>
+    <a href="#">Contact</a>
+  </nav>
+</div>
+
 
           <div id="animated-bg" />
 
