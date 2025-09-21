321fb218bb280921fa959221db6c0e4fb99949c3
atmukesh01 <iammukesh1075@gmail.com>
2025-09-21 10:48:27 +0530

    Modified

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -147,7 +147,7 @@ const Contact = ({ isPageLoading, onLoadingComplete }) => {
             I'm always open to discussing new projects, creative ideas, or opportunities to be part of your visions.
           </p>
           <div className="contact-links">
-            <a href="mailto:iammukesh1075@gmail.com" className="contact-link-item">
+            <a href="mailto:at.mukesh01@gmail.com" className="contact-link-item">
               <EmailIcon />
               <span>Email Me</span>
             </a>
