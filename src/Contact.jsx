75460af8168426ecde5029d6833f76a8a0cbd234
atmukesh01 <iammukesh1075@gmail.com>
2026-03-18 15:24:56 +0530

    Final

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -136,7 +136,7 @@ const Contact = ({ isPageLoading, onLoadingComplete }) => {
       <div className="contact-container">
         <div className="contact-left">
           <img
-            src="assets/blr.jpg"
+            src="assets/black.png"
             alt="Profile"
             className="contact-image"
           />
